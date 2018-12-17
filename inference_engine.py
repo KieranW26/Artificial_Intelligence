60cbe75256922e3898d4a677a71049dc0f3412b6
KieranW26 <44164830+KieranW26@users.noreply.github.com>
2018-12-17 17:41:35 +0000

    Add files via upload

--- inference_engine.py
+++ inference_engine.py
@@ -1,38 +1,53 @@
+import re
 from urllib.request import urlopen
 from lxml import html
 import requests
 from pyknow import *
 
 
-class Greetings(KnowledgeEngine):
-    @DefFacts()
-    def _initial_action(self):
-        yield Fact(action="greet")
+def inference(depart, arrive, time, date):
+    class TrainTickets(KnowledgeEngine):
+        @DefFacts()
+        def _initial_action(self):
+            yield Fact(action="route")
 
-    @Rule(Fact(action='greet'),
-          NOT(Fact(name=W())))
-    def ask_name(self):
-        self.declare(Fact(name=input("What's your name? ")))
+        @Rule(Fact(action='route'),
+              Fact(departing_station=MATCH.departing_station),
+              Fact(arriving_station=MATCH.arriving_station),
+              Fact(time=MATCH.time),
+              Fact(date=MATCH.date))
+        def route(self, departing_station, arriving_station, time, date):
+            url = "https://traintimes.org.uk/" + departing_station + "/" + arriving_station + "/" + time + "/" + date
+            page = requests.get(url)
+            tree = html.fromstring(page.content)
+            if 'a' in time:
+                time = time.replace('a', '')
+                print(departing_station + ' to ' + arriving_station + ' arriving at ' + time + ' on ' + date)
+            else:
+                print(departing_station + ' to ' + arriving_station + ' departing at ' + time + ' on ' + date)
+            for x in range(5):
+                printout = ''
+                times = tree.xpath('//li[@id="result' + str(x) + '"]/strong[1]/text()')
+                printout = printout + re.sub('[\[\'\]' ']', '', str(times))
+                platform_temp = tree.xpath('//li[@id="result' + str(x) + '"]/small/em/text()')
+                platform_temp = str(platform_temp).replace('\\n', '').replace('\\t', '').replace('Platform', 'Platform ')\
+                    .replace(';', ' ')
+                platform = re.sub('[^a-zA-Z\d\s:]', '', str(platform_temp))
+                if platform_temp:
+                    printout = printout + ' ' + platform + ' '
+                else:
+                    printout = printout + '             '
+                price = tree.xpath('//li[@id="result'+ str(x) + '"]/small[2]/span[@class="tooltip" and 1]/text()')
+                printout = printout + re.sub('[\[\'\]' ']', '', str(price))
+                print(printout)
 
-    @Rule(Fact(action='greet'),
-          NOT(Fact(location=W())))
-    def ask_location(self):
-        self.declare(Fact(location=input("Where are you? ")))
 
-    @Rule(Fact(action='greet'),
-          Fact(name=MATCH.name),
-          Fact(location=MATCH.location))
-    def greet(self, name, location):
-        url = "https://traintimes.org.uk/" + name + "/" + location
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
-        for x in range(5):
-            times = tree.xpath('//li[@id="result' + str(x) + '"]/strong[1]/text()')
-            print(times)
-            price = tree.xpath('//li[@id="result' + str(x) + '"]/small[2]/span[@class="tooltip" and 2]/text()')
-            print(price)
+    engine = TrainTickets()
+    engine.reset()  # Prepare the engine for the execution.
+    engine.declare(Fact(departing_station = depart))
+    engine.declare(Fact(arriving_station = arrive))
+    engine.declare(Fact(time = time))
+    engine.declare(Fact(date = date))
+    engine.run()  # Run it!
 
-
-engine = Greetings()
-engine.reset()  # Prepare the engine for the execution.
-engine.run()  # Run it!
+inference('Norwich', 'London', '09:00', '2018-12-21')
