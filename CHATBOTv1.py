b6a5be7462bcf29b08ffac15dc98b862df573a28
JackPatten <32520003+Bethod@users.noreply.github.com>
2019-01-10 23:21:05 +0000

    Add files via upload

--- CHATBOTv1.py
+++ CHATBOTv1.py
@@ -7,25 +7,43 @@ Created on Sun Dec 30 16:43:51 2018
 from datetime import time
 from datetime import datetime
 import re
+import csv 
 
+class GACHATBOT:
+    
+    kb_from_station_name = "empty"
+    kb_to_station_name = "empty"
+    
+    kb_from_station_code = ""
+    kb_to_station_code = ""
+    
+    #GETS TODAY DATE AND TIME AND SEPERATES THEM
+    d = datetime.now().strftime("%Y-%m-%d %H:%M")
+    kb_date, space, kb_time = d.partition(' ')
+   # kb_date, space, kb_time = d.partition(' ')
+    dflag = 0
+    tflag = 0
+    
+    kb_return = False
+    kb_ret_date, space2, kb_ret_time = d.partition(' ')
+    
+    #kb_ret_date, space2, kb_ret_time = d.partition(' ') 
+    kb_command = '' 
+    
+    cb_flag = 0
+    cb_running = True
+    
+    
 
-class GACHATBOT: 
-    #Variables for Journey
-    START_STATION = "empty"
-    DESTINATION_STATION = "empty" 
-    DATE = datetime.today()
-    #Variables for return journies
-    RETURN = False 
-    RETURN_DATE = datetime.today()
-    #Variables for running of the system
-    RUNNING = True
-    CHECK_DATE = datetime.today() 
-    FLAG = 0
    
     
     #Key Words and Phrases  
-    TRAIN_STATIONS = ("norwich", "diss" ,
-                  "stowmarket", "bury" , "cambrdige")
+    TRAIN_STATIONS = ["diss", "norwich"]
+   # data = csv.reader(open("norfolk_stations.csv"))
+    #for line in data:
+     #   TRAIN_STATIONS.append(re.sub(r'[^a-zA-Z0-9 ]+', '', str(line).lower()))
+      #  print(line)
+        
     OPENING_PHRASES = ("Hello, I am TrainBot how could I help?")
     MONTHS = ("january","february","march", "april", "may", "june", "july", "august", "september", "october", "november" , "decemeber")
     TIME_CONTAIN = ("am" , "pm")
@@ -33,24 +51,48 @@ class GACHATBOT:
     #REGEX 
     DATE_REGEX = ('\d{2}/\d{2}/\d{4}')
     TIME_REGEX = ('^(([01]\d|2[0-3]):([0-5]\d)|24:00)$')
+
+  
+    
     
     def runBot(self): 
-        while self.RUNNING is True :
+        while self.cb_running is True :
+            
+            print( self.kb_from_station_name, self.kb_to_station_name, self.kb_date, self.kb_time, self.kb_return  )
+            if self.kb_return is True:
+                print(self.kb_ret_date, self.kb_ret_time)
             userInput = input("USER: ")
            # print(self.DATE)
             #self.DATE = self.DATE.replace(year = 2017, hour = 13)
             #print(self.DATE)
             if userInput == "stop":
-                self.RUNNING = False
+                self.cb_running = False
             else: 
                 
-                if self.FLAG == 1: 
+                if self.cb_flag == 1: 
                     print("")
                     if "yes" in userInput: 
+                        # WHERE THE PROGRAM CALLS THE FIND TRAIN TICKET FUNCTION
+                        # RENAME IF NEEDED - JACK
                         self.findTrainTimes()
+                        self.kb_from_station_name = "empty"
+                        self.kb_to_station_name = "empty" 
+                        self.kb_date, self.space, self.kb_time = self.d.partition(' ')
+                        self.kb_return = False 
+                        self.kb_ret_date, self.space, self.kb_time = self.d.partition(' ')
+                        self.cb_flag = 0
+                        self.dflag = 0
+                        self.tflag = 0
                     else:
-                        self.clear()
-                        print("okay, lets try that again, what is your jounrey?")
+                        self.kb_from_station_name = "empty"
+                        self.kb_to_station_name = "empty" 
+                        self.kb_date, self.space, self.kb_time = self.d.partition(' ')
+                        self.kb_return = False 
+                        self.kb_ret_date, self.space, self.kb_time = self.d.partition(' ')
+                        self.cb_flag = 0
+                        self.dflag = 0
+                        self.tflag = 0 
+                        print("okay, lets try that again, what is your journey?")
                 else: 
                     self.listen(userInput)
                     self.respond(userInput)
@@ -60,33 +102,27 @@ class GACHATBOT:
     def listen(self, sentence): 
         words = sentence.split()
         if "return" in sentence.lower(): 
-            self.RETURN = True
+            self.kb_return = True
         for counter, word in enumerate(words):
            
             if word.lower() in self.TRAIN_STATIONS:
-                if words[counter-1] is "to" or self.START_STATION is not "empty":
-                    self.DESTINATION_STATION = word.lower() 
-                    print(self.DESTINATION_STATION)
+                if words[counter-1] is "to" or self.kb_from_station_name is not "empty":
+                    self.kb_to_station_name = word.lower()
                 else:
-                    self.START_STATION = word.lower() 
-                    print(self.START_STATION)
+                    self.kb_from_station_name = word.lower() 
+                    print(self.kb_from_station_name)
                     #date capture - only 1 method implemented with format "dd/mm/yy"
-            elif re.search(self.DATE_REGEX, word):
-                sday, smonth, syear = word.split("/")      
-                self.DATE = self.DATE.replace(day = int(sday), month = int(smonth), year = int(syear))
-            elif  re.search(self.TIME_REGEX, word):
-                hh, mm = word.split(":")
-                print(hh)
-                self.DATE = self.DATE.replace(hour=int(hh), minute=int(mm))
-                print(self.DATE)
-            elif self.RETURN is True: 
+            elif re.search(self.DATE_REGEX, word) and self.dflag == 0:
+                self.dflag = 1
+                self.kb_date = word
+            elif  re.search(self.TIME_REGEX, word) and self.tflag == 0:
+                self.tflag = 1
+                self.kb_time = word         
+            elif self.kb_return is True: 
                 if re.search(self.DATE_REGEX, word):
-                    sday, smonth, syear = word.split("/")
-                    self.DATE = self.RETURN_DATE = datetime.replace(day = int(sday), month = int(syear), year = int(syear))
+                    self.kb_ret_date = word
                 elif re.search(self.TIME_REGEX, word):
-                     hh, mm = word.split(":")
-                     self.DATE = self.DATE.replace(hour = int(hh), minute = int(mm))
-                    
+                    self.kb_ret_time = word
                 
       
             
@@ -94,9 +130,9 @@ class GACHATBOT:
     def respond(self, setence):
         start = "BOT: " 
         answer = "You still need to enter your" 
-        if self.START_STATION == "empty":
+        if self.kb_from_station_name == "empty":
             answer = answer + " starting station " 
-        if self.DESTINATION_STATION == "empty":
+        if self.kb_to_station_name == "empty":
             if answer == "You still need to enter your":
                 answer = answer + " destination station "
             else: 
@@ -106,31 +142,26 @@ class GACHATBOT:
         if answer != "You still need to enter your":
             print(start + answer )
         else: 
-            response = "You are traveling from " + self.START_STATION + " to " + self.DESTINATION_STATION + " on " 
-            response = response + str(self.DATE.day) + "/" + str(self.DATE.month) + "/" + str(self.DATE.year) + " at " 
-            response = response + str(self.DATE.hour) + ":" + str(self.DATE.minute) 
-            if self.RETURN:
+            response = "You are traveling from " + self.kb_from_station_name + " to " + self.kb_to_station_name + " on " 
+            response = response + self.kb_date + " at " +  self.kb_time
+            
+            if self.kb_return:
                 response = response + " and returning on the " 
-                if self.RETURN_DATE.month == self.DATE.month and self.RETURN_DATE.day == self.DATE.day:
-                    response = response + "same day at" 
+                if self.kb_ret_date == self.kb_date:
+                    response = response + " same day at " + self.kb_ret_time
                 else:
-                    response = response + str(self.DATE.day) + "/" + str(self.DATE.month) + "/" + str(self.DATE.year)
+                    response = response + self.kb_ret_date + " at " + self.kb_ret_time
             print(response + ". Is that Correct?")
-            self.FLAG +=1
+            self.cb_flag +=1
    
     
     def findTrainTimes(self):
         
         print("") 
     
-    def clear(self):
-        self.START_STATION = "empty"
-        self.DESTINATION_STATION = "empty" 
-        self.DATE = datetime.today()
-        self.RETURN = False 
-        self.RETURN_DATE = datetime.today()
-        self.FLAG = 0
-
+     
+          
+   
 
 
 
