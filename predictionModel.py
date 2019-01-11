4aa8e95df4cb9f9bf455aadbaaa6b20c8b730818
AshleyBlack <A.Black@uea.ac.uk>
2019-01-11 13:26:22 +0000

    Add files via upload

--- predictionModel.py
+++ predictionModel.py
@@ -12,13 +12,13 @@ class PredictionModel():
         # late or not
         self.weights = 2 * np.random.random((3, 1)) - 1 
 
-    # applies the sigmoid function
+    # applies the sigmoid function to normalise sum of the inputs
     def sigmoid(self, x):
         
         return 1 / (1 + np.exp(-x))
 
 
-    # calculates the derivative to the sigmoid function above  
+    # calculates the derivative to the sigmoid function above for weight adjustments
     def sigmoidDerivative(self, x):
         
         return x * (1 - x)
@@ -26,12 +26,12 @@ class PredictionModel():
 
     # trains the prediction model to understand and learn the outcomes of the train
     # routes based on the information given by the 2 arrays mentioned above   
-    def train(self, routeInformation, routeLate, trainingIterations):
+    def weightAdjustment(self, routeInformation, routeLate, trainingIterations):
         
         for iteration in range(trainingIterations):
             
             # iterates through each instance of a train route information
-            output = self.think(routeInformation)
+            output = self.prediction(routeInformation)
 
             # computes the back propogation outcome
             error = routeLate - output
@@ -44,7 +44,7 @@ class PredictionModel():
             
     # passes new data through the instances in the model to receive accurate
     # prediction based on the weights calculated earlier  
-    def think(self, inputs):
+    def prediction(self, inputs):
         
         inputs = inputs.astype(float)
         output = self.sigmoid(np.dot(inputs, self.weights))
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         
         time = 0
     
-    if trainDeparture == 'london':
+    if trainDeparture == 'acle':
         
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -126,11 +126,11 @@ if __name__ == "__main__":
                                       0     , 1     , 0     , 0     , 1   ,
                                       1     , 0     , 1     , 0     , 0]]).T
     
-        delayTimes = {5, 12, 7, 1, 27, 11, 29, 34, 16, 18, 28, 4, 16, 34, 22, 
-                      6, 3, 29, 6, 19, 22, 48, 17, 53, 2, 54, 13, 17, 3, 6, 4,
-                      8, 12, 67, 17, 42, 12, 53, 36, 31, 5, 8, 16, 14, 5, 6, 2}
+        delayTimes = {5, 2, 7, 1, 7, 11, 2, 3, 1, 10, 8, 4, 6, 3, 2, 
+                      6, 3, 9, 6, 1, 2, 8, 17, 5, 2, 4, 1, 7, 3, 6, 4,
+                      8, 12, 6, 7, 2, 12, 3, 6, 1, 5, 8, 6, 14, 5, 6, 2}
         
-    elif trainDeparture == 'norwich':
+    elif trainDeparture == 'attleborough':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -150,10 +150,10 @@ if __name__ == "__main__":
                                       1     , 0     , 1     , 0     , 0]]).T
     
         delayTimes = {10, 2, 5, 8, 3, 12, 4, 5, 2, 2, 15, 35, 15, 3, 17, 31, 26,
-                      4, 16, 4, 21, 4, 1, 41, 24, 6, 3, 7, 16, 14, 4, 7, 11, 5,
-                      7, 2, 9, 4, 16, 21, 20, 10, 4, 20, 5}
+                      4, 16, 4, 21, 4, 1, 4, 4, 6, 3, 7, 6, 4, 4, 7, 11, 5,
+                      7, 2, 9, 4, 1, 1, 2, 1, 4, 2, 5}
     
-    elif trainDeparture == 'cambridge':
+    elif trainDeparture == 'beccles':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -173,10 +173,10 @@ if __name__ == "__main__":
                                       1     , 0     , 0     , 0     , 0]]).T
     
         delayTimes = {1, 3, 3, 6, 2, 3, 4, 8, 1, 3, 6, 3, 9, 12, 3, 8, 10, 3, 1,
-                      6, 18, 4, 8, 23, 36, 4, 7, 14, 20, 17, 5, 3, 9, 10, 3, 9,
-                      4, 6, 13, 36, 5, 4, 8, 8, 5, 7, 14, 4, 2, 5, 6, 6, 2}
+                      6, 8, 4, 8, 2, 6, 4, 7, 4, 2, 7, 5, 3, 9, 10, 3, 9,
+                      4, 6, 13, 6, 5, 4, 8, 8, 5, 7, 4, 4, 2, 5, 6, 6, 2}
         
-    elif trainDeparture == 'manchester':
+    elif trainDeparture == 'cambridge':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -195,11 +195,11 @@ if __name__ == "__main__":
                                       0     , 1     , 0     , 0     , 0   ,
                                       0     , 0     , 0     , 1     , 1]]).T
     
-        delayTimes = {12, 4, 16, 2, 5, 18, 27, 43, 16, 25, 32, 4, 14, 10, 35, 9,
-                      26, 20, 8, 14, 6, 12, 10, 18, 25, 30, 26, 13, 8, 16, 35,
-                      8, 4, 24, 10, 34, 16, 5, 8, 9, 16, 2, 6, 3, 26, 3, 8, 10}
+        delayTimes = {12, 4, 6, 2, 5, 8, 7, 4, 6, 5, 3, 4, 4, 10, 5, 9,
+                      2, 20, 8, 14, 6, 12, 10, 8, 5, 3, 6, 13, 8, 16, 5,
+                      8, 4, 2, 10, 4, 16, 5, 8, 9, 6, 2, 6, 3, 6, 3, 8, 10}
     
-    elif trainDeparture == 'newcastle':
+    elif trainDeparture == 'diss':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -218,11 +218,11 @@ if __name__ == "__main__":
                                       0     , 0     , 0     , 1     , 1   ,
                                       0     , 1     , 0     , 0     , 0]]).T
     
-        delayTimes = {9, 24, 18, 7, 29, 21, 7, 18, 35, 19, 23, 22, 10, 4, 6, 4,
-                      18, 22, 20, 13, 4, 8, 2, 19, 6, 7, 3, 5, 12, 6, 5, 4, 9,
-                      4, 9, 19, 24, 43, 1, 4, 15, 7, 38, 47, 12, 30, 3, 6, 1, 3}
+        delayTimes = {9, 4, 8, 7, 9, 1, 7, 8, 5, 9, 3, 2, 10, 4, 6, 4,
+                      18, 22, 2, 13, 4, 8, 2, 9, 6, 7, 3, 5, 12, 6, 5, 4, 9,
+                      4, 9, 9, 4, 4, 1, 4, 5, 7, 8, 4, 2, 3, 3, 6, 1, 3}
     
-    elif trainDeparture == 'bristol':
+    elif trainDeparture == 'ipswich':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -241,11 +241,11 @@ if __name__ == "__main__":
                                       0     , 0     , 0     , 1     , 0   ,
                                       0     , 1     , 0     , 1     , 0]]).T
     
-        delayTimes = {4, 16, 2, 8, 25, 12, 8, 4, 26, 31, 10, 5, 25, 4, 8, 2, 7, 
-                      4, 8, 1, 21, 18, 10, 34, 6, 9, 3, 10, 4, 8, 22, 13, 10, 
-                      4, 19, 10, 36, 2, 7, 4, 9, 15, 5}
+        delayTimes = {4, 6, 2, 8, 5, 12, 8, 4, 6, 3, 10, 5, 5, 4, 8, 2, 7, 
+                      4, 8, 1, 21, 8, 10, 4, 6, 9, 3, 10, 4, 8, 22, 13, 10, 
+                      4, 9, 10, 6, 2, 7, 4, 9, 5, 5}
     
-    elif trainDeparture == 'leeds':
+    elif trainDeparture == 'norwich':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -264,11 +264,11 @@ if __name__ == "__main__":
                                       1     , 1     , 0     , 0     , 1   ,
                                       1     , 1     , 0     , 1     , 0]]).T
     
-        delayTimes = {3, 8, 5, 17, 13, 5, 29, 5, 18, 10, 25, 20, 24, 3, 2, 6, 
-                      4, 9, 10, 34, 21, 3, 8, 10, 3, 6, 8, 8, 6, 5, 7, 3, 12,
-                      10, 5, 23, 22, 20, 18, 4, 7, 2, 9, 5, 14, 19, 23, 12, 10}
+        delayTimes = {3, 8, 5, 17, 13, 5, 9, 5, 8, 10, 5, 2, 2, 3, 2, 6, 
+                      4, 9, 10, 3, 2, 3, 8, 10, 3, 6, 8, 8, 6, 5, 7, 3, 12,
+                      10, 5, 3, 2, 2, 8, 4, 7, 2, 9, 5, 14, 9, 3, 12, 10}
     
-    elif trainDeparture == 'birmingham':
+    elif trainDeparture == 'peterborough':
             
         routeInformation = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],    
                                      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
@@ -287,16 +287,16 @@ if __name__ == "__main__":
                                       0     , 1     , 0     , 0     , 1   ,
                                       1     , 0     , 1     , 1     , 0]]).T
     
-        delayTimes = {6, 2, 9, 14, 10, 4, 8, 19, 4, 6, 21, 10, 7, 3, 20, 9, 15,
-                      8, 14, 9, 10, 4, 16, 28, 35, 2, 6, 9, 13, 15, 20, 16, 4,
-                      9, 25, 16, 4, 19, 24, 31, 19, 6, 34, 5, 1, 7, 4, 12, 6}
+        delayTimes = {6, 2, 9, 14, 10, 4, 8, 9, 4, 6, 1, 1, 7, 3, 2, 9, 5,
+                      8, 14, 9, 10, 4, 6, 8, 3, 2, 6, 9, 13, 5, 2, 6, 4,
+                      9, 5, 6, 4, 9, 2, 3, 9, 6, 4, 5, 1, 7, 4, 12, 6}
 
     
-    trainPredictions.train(routeInformation, routeLate, 15000)
+    trainPredictions.weightAdjustment(routeInformation, routeLate, 15000)
         
-    prediction = trainPredictions.think(np.array([day, stops, time]))
+    prediction = trainPredictions.prediction(np.array([day, stops, time]))
     percentage = prediction * 100
-    print("There is a " , percentage , "% chance that your train will be delayed")
+    print("There is up to a " , percentage , "% chance that your train will be delayed")
     
     predictedDelay = trainPredictions.timeMean(delayTimes)
     print("Your predicted train delay is " , predictedDelay , " minutes, should one occur.")
